7fc12975f6e6c4a37055da0a77161784100fb4dd
josue0ghost <llaajjee@hotmail.com>
2018-04-05 07:40:50 -0600

    ggx2

---
+++ Lab4/Lab4/Models/Equipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab4.Models
+{
+    public class Equipo
+    {
+        List<int> Faltantes;
+        List<int> Coleccionadas;
+        List<int> Cambios;
+    }
+}
